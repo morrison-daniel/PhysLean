1d7eeb7c4e7c736b42cdb36cf04b789a331d0dd5
jstoobysmith <72603918+jstoobysmith@users.noreply.github.com>
2025-03-21 11:04:47 -0400

    feat: Boosts of Lorentz vectors

--- PhysLean/Relativity/Lorentz/RealTensor/Vector/Basic.lean
+++ PhysLean/Relativity/Lorentz/RealTensor/Vector/Basic.lean
@@ -31,6 +31,10 @@ abbrev Vector (d : ℕ := 3) := ℝT[d, .up]
 
 namespace Vector
 
+set_option quotPrecheck false in
+/-- The actoin of the Lorentz group on a Lorentz vector. -/
+scoped infixl:60 "•" => ((realLorentzTensor _).F.obj (OverColor.mk ![Color.up])).ρ
+
 /-- The equivalence between the type of indices of a Lorentz vector and
   `Fin 1 ⊕ Fin d`. -/
 def indexEquiv {d : ℕ} :
@@ -71,6 +75,17 @@ lemma toCoord_injective {d : ℕ} : Function.Injective (@toCoord d) := by
   exact toCoord.toEquiv.injective
 
 instance : CoeFun (Vector d) (fun _ => Fin 1 ⊕ Fin d → ℝ) := ⟨toCoord⟩
+
+lemma toCoord_tprod {d : ℕ} (p : (i : Fin 1) →
+    ↑((realLorentzTensor d).FD.obj { as := ![Color.up] i }).V) (i : Fin 1 ⊕ Fin d) :
+    toCoord (PiTensorProduct.tprod ℝ p) i =
+      ((Lorentz.contrBasisFin d).repr (p 0))
+      (indexEquiv.symm i 0) := by
+  rw [toCoord_apply]
+  simp
+  erw [TensorSpecies.TensorBasis.tensorBasis_repr_tprod]
+  simp
+  rfl
 
 lemma tensorNode_repr_apply {d : ℕ} (p : Vector d)
     (b : (j : Fin (Nat.succ 0)) → Fin ((realLorentzTensor d).repDim (![Color.up] j))) :
@@ -155,10 +170,10 @@ lemma innerProduct_zero_right {d : ℕ} (p : Vector d) :
 
 @[simp]
 lemma innerProduct_invariant {d : ℕ} (p q : Vector d) (Λ : LorentzGroup d) :
-    ⟪((realLorentzTensor d).F.obj _).ρ Λ p, ((realLorentzTensor d).F.obj _).ρ Λ q⟫ₘ = ⟪p, q⟫ₘ := by
+    ⟪Λ • p, Λ • q⟫ₘ = ⟪p, q⟫ₘ := by
   rw [innerProduct]
   have h1 (q : Vector d) :
-    (tensorNode ((((realLorentzTensor d).F.obj (OverColor.mk ![Color.up])).ρ Λ) q)).tensor
+    (tensorNode (Λ • q)).tensor
     = (action Λ (tensorNode q)).tensor := by simp [action_tensor]
   rw [field]
   rw [contr_tensor_eq <| prod_tensor_eq_snd <| h1 q]
@@ -252,6 +267,63 @@ def asSmoothManifold (d : ℕ) : ModelWithCorners ℝ (Vector d) (Vector d) := �
 instance : ChartedSpace (Vector d) (Vector d) := chartedSpaceSelf (Vector d)
 
 end smoothness
+
+/-!
+
+## The Lorentz action
+
+-/
+
+lemma action_apply_eq_sum (i : Fin 1 ⊕ Fin d) (Λ : LorentzGroup d) (p : Vector d) :
+    (Λ • p) i = ∑ j, Λ.1 i j * p j := by
+  revert p
+  refine fun p ↦
+    PiTensorProduct.induction_on' p ?_ (by
+    intro a b hx hy
+    simp at hx hy ⊢
+    rw [hx, hy]
+    simp [mul_add, Finset.sum_add_distrib]
+    ring)
+  intro r p
+  simp only [C_eq_color, TensorSpecies.F_def, Nat.succ_eq_add_one, Nat.reduceAdd, OverColor.mk_left,
+    Functor.id_obj, OverColor.mk_hom, PiTensorProduct.tprodCoeff_eq_smul_tprod, _root_.map_smul,
+    Pi.smul_apply, smul_eq_mul]
+  erw [OverColor.lift.objObj'_ρ_tprod]
+  conv_rhs =>
+    enter [2, x]
+    rw [← mul_assoc, mul_comm _ r, mul_assoc]
+  rw [← Finset.mul_sum]
+  congr
+  have hl : (𝟭 Type).obj (OverColor.mk ![Color.up]).left = Fin 1 := by rfl
+  simp_all only [Nat.succ_eq_add_one, OverColor.mk_left, _root_.zero_add, Functor.id_obj,
+    C_eq_color, OverColor.mk_hom]
+  erw [toCoord_tprod]
+  change ((contrBasisFin d).repr (Λ *ᵥ _)) _ = _
+  rw [contrBasisFin_repr_apply]
+  rw [ContrMod.mulVec_val]
+  rw [mulVec_eq_sum]
+  simp only [Fin.isValue, op_smul_eq_smul, Nat.succ_eq_add_one, Nat.reduceAdd, Finset.sum_apply,
+    Pi.smul_apply, transpose_apply, smul_eq_mul]
+  congr
+  funext j
+  rw [mul_comm]
+  congr
+  · match i with
+    | Sum.inl 0 => rfl
+    | Sum.inr i => simp [finSumFinEquiv, indexEquiv]
+  · erw [toCoord_tprod]
+    rw [contrBasisFin_repr_apply]
+    congr
+    match j with
+    | Sum.inl 0 => rfl
+    | Sum.inr j => simp [finSumFinEquiv, indexEquiv]
+
+lemma action_toCoord_eq_mulVec {d} (Λ : LorentzGroup d) (p : Vector d) :
+    toCoord (Λ • p) = Λ.1 *ᵥ (toCoord p) := by
+  funext i
+  rw [action_apply_eq_sum, mulVec_eq_sum]
+  simp only [op_smul_eq_smul, Finset.sum_apply, Pi.smul_apply, transpose_apply, smul_eq_mul,
+    mul_comm]
 
 end Vector
 
