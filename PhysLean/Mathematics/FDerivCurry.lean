2680ecda65714d246499158a468edc18c3a35be5
ZhiKai Pong <zhikaip@gmail.com>
2025-04-26 18:42:04 +0100

    author name order

--- PhysLean/Mathematics/FDerivCurry.lean
+++ PhysLean/Mathematics/FDerivCurry.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2025 Joseph Tooby-Smith. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Joseph Tooby-Smith, Zhi Kai Pong, Tomáš Skřivan
+Authors: Zhi Kai Pong, Tomáš Skřivan, Joseph Tooby-Smith
 -/
 import Mathlib.Analysis.Calculus.FDeriv.Mul
 import Mathlib.Analysis.Calculus.FDeriv.Symmetric
