a937a8f7e8f9557d157c5f280ab5e4381158ab6e
ZhiKai Pong <zhikaip@gmail.com>
2025-05-01 22:29:20 +0100

    extra space

--- PhysLean/ClassicalMechanics/Time/Basic.lean
+++ PhysLean/ClassicalMechanics/Time/Basic.lean
@@ -44,7 +44,7 @@ scoped notation "∂ₜ" => deriv
 
 lemma deriv_smul (f : Time → EuclideanSpace ℝ (Fin 3)) (k : ℝ)
     (hf : Differentiable ℝ f) :
-    ∂ₜ (fun t => k • f t) t  = k • ∂ₜ (fun t => f t) t := by
+    ∂ₜ (fun t => k • f t) t = k • ∂ₜ (fun t => f t) t := by
   unfold deriv
   rw [fderiv_const_smul]
   rfl
