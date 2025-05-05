254807dd11e6f17f20f6cb9fb531a8da8be462db
Joseph Tooby-Smith <72603918+jstoobysmith@users.noreply.github.com>
2025-05-05 06:01:21 +0000

    feat: Some small properties of tight binding (#537)
    * feat: TODO related to tight binding chain
    
    * refactor: Lint
    
    * Update Basic.lean
    
    * feat: Small change to Tight binding chain
    
    * refactor: Lint
    
    * refactor: Lint

--- PhysLean/CondensedMatter/TightBindingChain/Basic.lean
+++ PhysLean/CondensedMatter/TightBindingChain/Basic.lean
@@ -4,6 +4,10 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Joseph Tooby-Smith
 -/
 import PhysLean.Meta.TODO.Basic
+import Mathlib.Analysis.InnerProductSpace.PiL2
+import PhysLean.Meta.Informal.Basic
+import PhysLean.Meta.Informal.SemiFormal
+import Mathlib.Analysis.Calculus.FDeriv.Symmetric
 /-!
 
 # The tight binding chain
@@ -13,6 +17,9 @@ in a 1d solid with the assumption the electron can sit only on the atoms of the 
 
 The solid is assumed to consist of `N` sites with a seperation of `a` between them.
 
+Mathematically, the tight binding chain corresponds to a
+QM problem located on a lattice with only self and nearest neighbour integractions.
+
 ## Refs.
 
 - https://www.damtp.cam.ac.uk/user/tong/aqm/aqmtwo.pdf
@@ -20,5 +27,45 @@ The solid is assumed to consist of `N` sites with a seperation of `a` between th
 -/
 
 TODO "BBZAB" "Prove results related to the one-dimensional tight binding chain.
-    This is related to the following issue/feature-request:
-    https://github.com/HEPLean/PhysLean/issues/523 "
+  This is related to the following issue/feature-request:
+  https://github.com/HEPLean/PhysLean/issues/523 "
+
+namespace CondensedMatter
+
+/-- The physical parameters making up the tight binding chain. -/
+structure TightBindingChain  where
+  /-- The number of sites, or atoms, in the chain -/
+  N : Nat
+  /-- The distance between the sites -/
+  a : ℝ
+  /-- The energy associate with a particle sitting at a fixed site. -/
+  E0 : ℝ
+  /-- The hopping parameter. -/
+  t : ℝ
+
+namespace TightBindingChain
+open InnerProductSpace
+variable (T : TightBindingChain)
+
+TODO "BQS7X" "The definition of the Hilbert space in the tight binding chian
+  should be moved to the generic folder for finite-dimensional quantum mechanical systems."
+
+/-- The Hilbert space of a `TightBindingchain` is the `N`-dimensional finite dimensional
+Hilbert space. -/
+abbrev HilbertSpace := EuclideanSpace ℂ (Fin T.N)
+
+/-- The eigenstate corresponding to the particle been located on the `n`th site. -/
+noncomputable def localizedState {T : TightBindingChain} (n : Fin T.N) : HilbertSpace T :=
+  EuclideanSpace.single n 1
+
+@[inherit_doc localizedState]
+scoped notation "|" n "⟩" => localizedState n
+
+/-- The inner product of two localized states. -/
+scoped notation "⟨" m "|" n "⟩" => ⟪localizedState m, localizedState n⟫_ℝ
+
+/-- The localized states are normalized. -/
+semiformal_result "BQSYT" localizedState_normalized (n : Fin T.N) : ⟨n|n⟩ = 1
+
+end TightBindingChain
+end CondensedMatter
