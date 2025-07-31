ade3eb093d39e7c64c59919e0c301cbb5a93b352
Joseph Tooby-Smith <72603918+jstoobysmith@users.noreply.github.com>
2025-07-31 15:33:02 +0000

    feat: Update linter warnings (#675)

--- scripts/lint_all.lean
+++ scripts/lint_all.lean
@@ -4,9 +4,10 @@ Released under Apache 2.0 license.
 Authors: Joseph Tooby-Smith
 -/
 
-def main (_: List String) : IO UInt32 := do
+def main (args : List String) : IO UInt32 := do
 
   println! "\x1b[36m- Style lint \x1b[0m"
+  println! "\x1b[2mThis linter is not checked by GitHub but if you have time please fix these errors.\x1b[0m"
   let styleLint ← IO.Process.output {cmd := "lake", args := #["exe", "style_lint"]}
   println! styleLint.stdout
 
@@ -27,12 +28,18 @@ def main (_: List String) : IO UInt32 := do
   let todoCheck ← IO.Process.output {cmd := "lake", args := #["exe", "check_dup_tags"]}
   println! todoCheck.stdout
 
-  println! "\x1b[36m- Transitive imports \x1b[0m"
-  let todoCheck ← IO.Process.output {cmd := "lake", args := #["exe", "redundent_imports"]}
-  println! todoCheck.stdout
+  if ¬ "--fast" ∈ args then
+    println! "\x1b[36m- Transitive imports \x1b[0m"
+    println! "\x1b[2mExpect this linter may take a while to run, it can be skipped with
+        lake exe lint_all --fast\x1b[0m"
+    let redundentImports ← IO.Process.output {cmd := "lake", args := #["exe", "redundent_imports"]}
+    println! redundentImports.stdout
 
-  println! "\x1b[36m-Lean linter \x1b[0m"
-  let leanCheck ← IO.Process.output {cmd := "lake", args := #["exe", "runLinter", "PhysLean"]}
-  println! leanCheck.stdout
+    println! "\x1b[36m- Lean linter \x1b[0m"
+    println! "\x1b[2mExpect this linter may take a while to run, it can be skipped with
+      lake exe lint_all --fast"
+    println! "You can manually perform this linter by placing `#lint` at the end of the files you have modified.\x1b[0m"
+    let leanCheck ← IO.Process.output {cmd := "lake", args := #["exe", "runLinter", "PhysLean"]}
+    println! leanCheck.stdout
 
   pure 0
