1a1c961075be055deddcc92bfa445bfdeb4bce87
Joseph Tooby-Smith <72603918+jstoobysmith@users.noreply.github.com>
2025-08-04 05:09:09 +0000

    docs: Reorder author list

--- PhysLean/SpaceAndTime/Space/SpaceStruct.lean
+++ PhysLean/SpaceAndTime/Space/SpaceStruct.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2025 Joseph Tooby-Smith. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Joseph Tooby-Smith, Hou Run Feng
+Authors: Hou Run Feng, Joseph Tooby-Smith
 -/
 import Mathlib.MeasureTheory.Measure.Haar.InnerProductSpace
 /-!
