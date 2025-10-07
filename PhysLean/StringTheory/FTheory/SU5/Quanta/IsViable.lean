85834f0cf3693a9facb29f84f82bcc9c74ac2b1e
Joseph Tooby-Smith <72603918+jstoobysmith@users.noreply.github.com>
2025-10-07 05:03:57 +0100

    feat(StringTheory): Modify IsViable Quanta (#762)

--- PhysLean/StringTheory/FTheory/SU5/Quanta/IsViable.lean
+++ PhysLean/StringTheory/FTheory/SU5/Quanta/IsViable.lean
@@ -4,6 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Joseph Tooby-Smith
 -/
 import PhysLean.StringTheory.FTheory.SU5.Charges.AnomalyFree
+import PhysLean.Particles.SuperSymmetry.SU5.ChargeSpectrum.ZMod
 /-!
 
 # Viable Quanta with Yukawa
@@ -40,6 +41,7 @@ We also write down the explicit set of viable quanta, and prove that this set is
   - B.2. A quanta is viable if and only if it is in the multiset
   - B.3. Every element of the multiset regenerates Yukawa at two insertions of the Yukawa singlets
   - B.4. Those quanta which satisfy the quartic anomaly cancellation condition
+  - B.5. Map down to Z2
 
 ## iv. References
 
@@ -64,22 +66,56 @@ open PotentialTerm ChargeSpectrum
 
 /-- For a given `I : CodimensionOneConfig` the condition on a `Quanta` for it to be
   phenomenologically viable. -/
-def IsViable (x : Quanta) : Prop :=
-  /- 1. Conditions just on the charges. -/
-  x.toCharges.IsComplete ∧
-  ¬ x.toCharges.IsPhenoConstrained ∧
-  ¬ x.toCharges.YukawaGeneratesDangerousAtLevel 1 ∧
-  (∃ I : CodimensionOneConfig, x.toCharges ∈ ofFinset I.allowedBarFiveCharges I.allowedTenCharges) ∧
-  x.F.toCharges.Nodup ∧
-  x.T.toCharges.Nodup ∧
-  AllowsTerm x.toCharges topYukawa ∧
-  /- 2. Conditions just on the fluxes. -/
-  x.F.toFluxesFive.NoExotics ∧
-  x.F.toFluxesFive.HasNoZero ∧
-  x.T.toFluxesTen.NoExotics ∧
-  x.T.toFluxesTen.HasNoZero ∧
-  /- 3. Conditions on the combination of the charges on the flux. -/
-  x.LinearAnomalyCancellation
+structure IsViable (x : Quanta) : Prop where
+  /-- There is a `Hd`, `Hu`, 5-bar matter and 10d matter. -/
+  has_all_charges : x.toCharges.IsComplete
+  /-- The charges do not lead to any phenomenologically constraining terms. -/
+  not_pheno_constrained : ¬ x.toCharges.IsPhenoConstrained
+  /-- The charges are such that dangerous couplings in the super potential are not
+    regenerated with the Yukawa terms. -/
+  not_regenerate_dangerous_couplings : ¬ x.toCharges.YukawaGeneratesDangerousAtLevel 1
+  /-- The charges are such that they are allowed by a configuration of sections on the
+    co-dimension 1 fiber. -/
+  charges_allowed_by_section_config :
+    (∃ I : CodimensionOneConfig, x.toCharges ∈ ofFinset I.allowedBarFiveCharges I.allowedTenCharges)
+  /-- Within the 5-bar matter, there is at most one entry for each charge. -/
+  no_duplicate_five_bar_charges : x.F.toCharges.Nodup
+  /-- Within the 10d matter, there is at most one entry for each charge. -/
+  no_duplicate_ten_charges : x.T.toCharges.Nodup
+  /-- The charges permit at least one Top-Yukawa coupling. -/
+  allows_top_yukawa : AllowsTerm x.toCharges topYukawa
+  /-- The fluxes of the 5-bar matter fields do not lead to any exotic chiral particles. -/
+  no_exotics_from_five_bar : x.F.toFluxesFive.NoExotics
+  /-- There are 5-bar matter fields where both fluxes are zero (these are not
+    being counted here). -/
+  no_five_bar_zero_fluxes : x.F.toFluxesFive.HasNoZero
+  /-- The fluxes of the 10d matter fields do not lead to any exotic chiral particles. -/
+  no_exotics_from_ten : x.T.toFluxesTen.NoExotics
+  /-- There are 10d matter fields where both fluxes are zero (these are not
+    being counted here). -/
+  no_ten_zero_fluxes : x.T.toFluxesTen.HasNoZero
+  /-- The quanta satisfy the linear anomaly cancellation conditions. -/
+  linear_anomalies : x.LinearAnomalyCancellation
+
+lemma isViable_iff_def (x : Quanta) : IsViable x ↔
+    x.toCharges.IsComplete ∧
+    ¬ x.toCharges.IsPhenoConstrained ∧
+    ¬ x.toCharges.YukawaGeneratesDangerousAtLevel 1 ∧
+    (∃ I : CodimensionOneConfig, x.toCharges ∈
+      ofFinset I.allowedBarFiveCharges I.allowedTenCharges) ∧
+    x.F.toCharges.Nodup ∧
+    x.T.toCharges.Nodup ∧
+    x.toCharges.AllowsTerm topYukawa ∧
+    x.F.toFluxesFive.NoExotics ∧
+    x.F.toFluxesFive.HasNoZero ∧
+    x.T.toFluxesTen.NoExotics ∧
+    x.T.toFluxesTen.HasNoZero ∧
+    x.LinearAnomalyCancellation := by
+  apply Iff.intro
+  · rintro ⟨h1, h2, h3, h4, h5, h6, h7, h8, h9, h10, h11, h12⟩
+    exact ⟨h1, h2, h3, h4, h5, h6, h7, h8, h9, h10, h11, h12⟩
+  · rintro ⟨h1, h2, h3, h4, h5, h6, h7, h8, h9, h10, h11, h12⟩
+    exact ⟨h1, h2, h3, h4, h5, h6, h7, h8, h9, h10, h11, h12⟩
 
 /-!
 
@@ -100,7 +136,7 @@ lemma isViable_iff_charges_mem_viableCharges (x : Quanta) :
         x.T.toFluxesTen.HasNoZero ∧
         /- 3. Conditions on the fluxes and the charges. -/
         x.LinearAnomalyCancellation := by
-  rw [IsViable]
+  rw [isViable_iff_def]
   conv_rhs =>
     enter [1, 1, I]
     rw [mem_viableCharges_iff']
@@ -252,6 +288,20 @@ lemma quarticAnomalyCancellation_iff_mem_of_isViable (x : Quanta) (h : IsViable 
     ⟨some (-2), some 2, {(-1, ⟨3, -2⟩), (1, ⟨0, 2⟩)}, {(1, ⟨3, 0⟩)}⟩,
     ⟨some 6, some (-14), {(-9, ⟨1, 2⟩), (1, ⟨2, -2⟩)}, {(-7, ⟨3, 0⟩)}⟩,
     ⟨some 6, some (-14), {(-9, ⟨0, 2⟩), (1, ⟨3, -2⟩)}, {(-7, ⟨3, 0⟩)}⟩} : Finset Quanta) := by
+  rw [isViable_iff_mem_viableElems] at h
+  revert x
+  decide
+
+/-!
+
+### B.5. Map down to Z2
+
+-/
+
+lemma map_to_Z2_of_isViable (x : Quanta ℤ) (h : IsViable x) :
+    x.toCharges.map (Int.castAddHom (ZMod 2)) ∈
+    ({⟨some 0, some 0, {1}, {1}⟩, ⟨some 0, some 0, {0,1}, {1}⟩, ⟨some 0, some 0, {0,1}, {0}⟩} :
+    Finset (ChargeSpectrum (ZMod 2))) := by
   rw [isViable_iff_mem_viableElems] at h
   revert x
   decide
