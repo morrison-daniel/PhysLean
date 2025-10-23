a04ed7195e61fa66cb7312e152775790155fe83e
luisgbariuan <luisgbariuan@gmail.com>
2025-10-22 21:36:36 -0700

    Formalize basic definitions in cosmology. Showed one intermediate lemma to prepare formalizing an informal lemma. (#775)
    * Added two lemmas. Showed one of them by rewriting of of sinh(k/x)x.
    
    * Added two lemmas cosmology
    
    * Formalized basic definitions in FLRW. Introduced intermediatte lemmata
    
    * Fixing errors from `./scripts/lint-style.sh`
    
    * reorder names so alphabetical
    
    * Move doc-string placement
    
    * run `#min_imports` at bottom of file
    
    * fix `lake exe lint_all`
    
    ---------
    
    Co-authored-by: jstoobysmith <72603918+jstoobysmith@users.noreply.github.com>

--- PhysLean/Cosmology/FLRW/Basic.lean
+++ PhysLean/Cosmology/FLRW/Basic.lean
@@ -1,42 +1,62 @@
 /-
 Copyright (c) 2025 Joseph Tooby-Smith. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Joseph Tooby-Smith
+Authors: Luis Gabriel C. Bariuan, Joseph Tooby-Smith
 -/
+import Mathlib.Analysis.Complex.Trigonometric
 import PhysLean.Meta.Informal.SemiFormal
 import PhysLean.SpaceAndTime.Space.Basic
 /-!
 
 # The Friedmann-Lemaître-Robertson-Walker metric
 
-Everything in this file is currently informal or semiformal.
+Parts of this file is currently informal or semiformal.
 
 -/
+
+open Filter
+open scoped Topology
 
 namespace Cosmology
 
 /-- The inductive type with three constructors:
 - `Spherical (k : ℝ)`
 - `Flat`
-- `Saddle (k : ℝ)` -/
-@[sorryful]
-def SpatialGeometry : Type := sorry
+- `Saddle (k : ℝ)`
+-/
+inductive SpatialGeometry : Type where
+  | Spherical (k : ℝ) (h : k < 0)
+  | Flat
+  | Saddle (k : ℝ) (h : k > 0)
 
 namespace SpatialGeometry
 
 /-- For `s` corresponding to
 - `Spherical k`, `S s r = k * sin (r / k)`
 - `Flat`, `S s r = r`,
-- `Saddle k`, `S s r = k * sin (r / k)`.
+- `Saddle k`, `S s r = k * sinh (r / k)`.
+-/
+noncomputable def S (s : SpatialGeometry) : ℝ → ℝ :=
+  fun r =>
+    match s with
+    | SpatialGeometry.Spherical k _ => k * Real.sin (r / k)
+    | SpatialGeometry.Flat => r
+    | SpatialGeometry.Saddle k _ => k * Real.sinh (r / k)
 
-Semiformal implementation note: There is likely a better name for this function. -/
+/-- The limit of `S (Saddle k) r` as `k → ∞` is equal to `S (Flat) r`.
+First show that `k * sinh(r / k) = sinh(r / k) / (1 / k)` pointwise. -/
+lemma mul_sinh_as_div (r k : ℝ) :
+    k * Real.sinh (r / k) = Real.sinh (r / k) / (1 / k) := by field_simp
+
+/-- First, show that limit of `sinh(r * x) / x` is r at the limit x goes to zero.
+Then the next theorem will address the rewrite using Filter.Tendsto.comp -/
 @[sorryful]
-def S (s : SpatialGeometry) : ℝ → ℝ := sorry
+lemma tendsto_sinh_rx_over_x (r : ℝ) :
+    Tendsto (fun x : ℝ => Real.sinh (r * x) / x) (𝓝[≠] 0) (𝓝 r) := by sorry
 
-/-- The limit of `S (Saddle k) r` as `k → ∞` is equal to `S (Flat) r`. -/
-informal_lemma limit_S_saddle where
-  deps := []
-  tag := "6ZZ3D"
+@[sorryful]
+lemma limit_S_saddle(r : ℝ) :
+    Tendsto (fun k : ℝ => k * Real.sinh (r / k)) atTop (𝓝 r) := by sorry
 
 /-- The limit of `S (Sphere k) r` as `k → ∞` is equal to `S (Flat) r`. -/
 informal_lemma limit_S_sphere where
